ea004a4d55f89e68f359edbdd41bf21cffba5d7d
KaiAiYu <robinzhang@exosite.com>
2018-09-27 22:53:52 +0800

    fix bug and words

--- app/src/main/java/com/cyut/toolbox/toolbox/changepassword.java
+++ app/src/main/java/com/cyut/toolbox/toolbox/changepassword.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class changepassword extends AppCompatActivity {
     String u_email;
     String psw;
     String psw2;
+    TextView view_err;
     String type = "changepsw";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +18,25 @@ public class changepassword extends AppCompatActivity {
         setContentView(R.layout.activity_changepassword);
         Bundle bundle = getIntent().getExtras();
         u_email = bundle.getString("u_email");
-
+        view_err= findViewById(R.id.view_err);
+        view_err.setText("");
     }
 
     public void resetpsw(View view) {
         psw = ((EditText) findViewById(R.id.psw)).getText().toString();
         psw2 = ((EditText) findViewById(R.id.psw2)).getText().toString();
-        Backgorundwork backgorundwork = new Backgorundwork(this);
-        backgorundwork.execute(type,u_email,psw,psw2);
+
+        if(psw.length() > 5){
+            if (psw.equals(psw2)) {
+                Backgorundwork backgorundwork = new Backgorundwork(this);
+                backgorundwork.execute(type,u_email,psw,psw2);
+            }else{
+                view_err.setText("檢查一下兩次輸入的密碼需相同唷");
+            }
+        }else{
+            view_err.setText("密碼需要大於(含)6個字唷");
+
+        }
     }
-}
+    }
+
