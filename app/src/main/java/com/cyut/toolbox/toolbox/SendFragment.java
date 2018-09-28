135e265e9ac3f89c4652808aa2892a74ca41472c
haru3613 <qaw8975889@gmail.com>
2018-09-28 17:25:51 +0800

    fuck github

--- app/src/main/java/com/cyut/toolbox/toolbox/SendFragment.java
+++ app/src/main/java/com/cyut/toolbox/toolbox/SendFragment.java
@@ -1,16 +1,26 @@
 package com.cyut.toolbox.toolbox;
 
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -34,13 +44,14 @@ import static android.content.Context.MODE_PRIVATE;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SendFragment extends Fragment {
+public class SendFragment extends Fragment implements SearchView.OnQueryTextListener{
     private View view;
     private RecyclerView recyclerView;
     private LinearLayoutManager layoutManager;
     private RecyclerViewAdapterMsg adapter;
     String uid;
     View v;
+    String SearchString;
     public static final String KEY = "STATUS";
     public SendFragment() {
         // Required empty public constructor
@@ -116,7 +127,11 @@ public class SendFragment extends Fragment {
         RequestQueue requestQueue = Volley.newRequestQueue(v.getContext());
         requestQueue.add(stringRequest);
     }
-
+    //Reload Fragment
+    public void Reload(){
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.detach(this).attach(this).commit();
+    }
 
     public void LoadUser(final String mail){
         String url ="http://163.17.5.182/loaduser.php";
@@ -159,6 +174,188 @@ public class SendFragment extends Fragment {
 
         };
         RequestQueue requestQueue = Volley.newRequestQueue(v.getContext());
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater) {
+        Log.d(TAG, "onCreateOptionsMenu: !");
+        inflater.inflate(R.menu.main, menu);
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView sv = new SearchView(((MainActivity) getActivity()).getSupportActionBar().getThemedContext());
+        MenuItemCompat.setShowAsAction(item, MenuItemCompat.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW | MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
+        MenuItemCompat.setActionView(item, sv);
+        sv.setOnQueryTextListener(this);
+        sv.setIconifiedByDefault(false);
+        sv.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        MenuItemCompat.setOnActionExpandListener(item, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                Reload();
+                return true;  // Return true to collapse action view
+            }
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+
+                return true;  // Return true to expand action view
+            }
+        });
+
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        SearchString=item.toString();
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                SearchV(SearchString);
+                return true;
+            case R.id.nav_1:
+                SearchV(SearchString);
+                Log.d(TAG, item.toString());
+                return true;
+            case R.id.nav_2:
+                SearchV(SearchString);
+                return true;
+            case R.id.nav_3:
+                SearchV(SearchString);
+                return true;
+            case R.id.nav_4:
+                SearchV(SearchString);
+                return true;
+            case R.id.nav_5:
+                SearchV(SearchString);
+                return true;
+            case R.id.nav_6:
+                SearchV(SearchString);
+                return true;
+            case R.id.nav_7:
+                Message(uid);
+                return true;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        SearchQuery(query);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if(!newText.equals("")){
+            //TODO 字串篩選
+        }
+        return false;
+    }
+
+    public void SearchV(final String SearchString){
+        String url ="http://163.17.5.182/search.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response:",response);
+                        try {
+                            byte[] u = response.getBytes(
+                                    "UTF-8");
+                            response = new String(u, "UTF-8");
+                            Log.d(TAG, "Response " + response);
+                            GsonBuilder builder = new GsonBuilder();
+                            Gson mGson = builder.create();
+                            List<ItemObject> posts = new ArrayList<ItemObject>();
+                            if (!response.contains("Undefined")) {
+                                posts = Arrays.asList(mGson.fromJson(response, ItemObject[].class));
+                                adapter = new RecyclerViewAdapterMsg(view.getContext(), posts);
+                                recyclerView.setAdapter(adapter);
+                            }
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //do stuffs with response erroe
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("search",SearchString);
+                params.put("uid",uid);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(view.getContext());
+        requestQueue.add(stringRequest);
+    }
+
+
+
+    public void SearchQuery(final String SearchString){
+        String url ="http://163.17.5.182/querysearch.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response:",response);
+                        try {
+                            byte[] u = response.getBytes(
+                                    "UTF-8");
+                            response = new String(u, "UTF-8");
+                            Log.d(TAG, "Response " + response);
+                            GsonBuilder builder = new GsonBuilder();
+                            Gson mGson = builder.create();
+                            List<ItemObject> posts = new ArrayList<ItemObject>();
+                            if (!response.contains("Undefined")) {
+                                posts = Arrays.asList(mGson.fromJson(response, ItemObject[].class));
+                                adapter = new RecyclerViewAdapterMsg(view.getContext(), posts);
+                                recyclerView.setAdapter(adapter);
+                            }else{
+                                Toast.makeText(getContext(),"沒有搜尋到相關案件",Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //do stuffs with response erroe
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("search",SearchString);
+                params.put("uid",uid);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(view.getContext());
         requestQueue.add(stringRequest);
     }
 
