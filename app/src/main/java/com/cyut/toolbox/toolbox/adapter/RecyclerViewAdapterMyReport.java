8e1504cee264206cfd86474bbffca49795c75934
haru3613 <qaw8975889@gmail.com>
2018-11-30 00:17:46 +0800

    1130

--- app/src/main/java/com/cyut/toolbox/toolbox/adapter/RecyclerViewAdapterMyReport.java
+++ app/src/main/java/com/cyut/toolbox/toolbox/adapter/RecyclerViewAdapterMyReport.java
@@ -110,17 +110,15 @@ public class RecyclerViewAdapterMyReport extends RecyclerView.Adapter<RecyclerVi
                 break;
         }
         if (itemList.get(position).getTitle().length()>8){
-            holder.Title.setText(itemList.get(position).getTitle().substring(0,8)+"...");
-        }else{
+            holder.Title.setText(itemList.get(position).getTitle().substring(0,8)+"\n"+itemList.get(position).getTitle().substring(8));
+        }else {
             holder.Title.setText(itemList.get(position).getTitle());
         }
 
         String Address=itemList.get(position).getCity()+itemList.get(position).getTown()+itemList.get(position).getRoad();
-        if (Address.length()>10){
-            holder.Area.setText(Address.substring(0,10)+"...");
-        }else{
-            holder.Area.setText(Address);
-        }
+
+        holder.Area.setText(Address);
+
 
 
         String lineSep = System.getProperty("line.separator");
